ad0d4acc7dccb8d7a0c79566e484ee3b0e256d0b
ariez <dpape39@icloud.com>
2026-03-28 09:34:55 +0100

    Tests: add skeleton test_estimation.py

---
+++ tests/test_estimation.py
@@ -0,0 +1,8 @@
+def test_uniform_weights_match_formula():
+    """Uniform weights should be 1/m in the sliding window and 0 everywhere else."""
+
+def test_history_estimator_delay():
+    """HistoryEstimator should trail the stream by the window radius."""
+
+def test_history_estimator_correctness():
+    """HistoryEstimator should return the average of the sliding window."""
